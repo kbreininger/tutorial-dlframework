26eb9e71d21b840a493425d0768bf570b0caa8ea
Benedikt Lorch <benedikt.lorch@fau.de>
2021-11-08 22:11:59 +0100

    MaxPoolLayer expects neighborhood as first argument and stride as second argument. Explicitly name arguments passed to MaxPoolLayer in test cases.

--- Tests/TestMaxPoolLayer.py
+++ Tests/TestMaxPoolLayer.py
@@ -23,25 +23,25 @@ class TestMaxPooling(unittest.TestCase):
         self.layers.append(Helpers.SoftMax())
 
     def test_shape(self):
-        layer = self.MaxPooling((2, 2), (2, 2))
+        layer = self.MaxPooling(neighborhood=(2, 2), stride=(2, 2))
         result = layer.forward(self.input_tensor)
         expected_shape = np.array([self.batch_size, 2, 2, 3])
         self.assertEqual(np.abs(np.sum(np.array(result.shape) - expected_shape)), 0)
 
     def test_overlapping_shape(self):
-        layer = self.MaxPooling((2, 1), (2, 2))
+        layer = self.MaxPooling(neighborhood=(2, 2), stride=(2, 1))
         result = layer.forward(self.input_tensor)
         expected_shape = np.array([self.batch_size, 2, 2, 6])
         self.assertEqual(np.abs(np.sum(np.array(result.shape) - expected_shape)), 0)
 
     def test_subsampling_shape(self):
-        layer = self.MaxPooling((3, 2), (2, 2))
+        layer = self.MaxPooling(neighborhood=(2, 2), stride=(3, 2))
         result = layer.forward(self.input_tensor)
         expected_shape = np.array([self.batch_size, 2, 1, 3])
         self.assertEqual(np.abs(np.sum(np.array(result.shape) - expected_shape)), 0)
 
     def test_gradient_stride(self):
-        self.layers[0] = self.MaxPooling((2, 2), (2, 2))
+        self.layers[0] = self.MaxPooling(neighborhood=(2, 2), stride=(2, 2))
         self.layers[2] = self.FullyConnected(12, self.categories, 0.)
 
         difference = Helpers.gradient_check(self.layers, self.input_tensor, self.label_tensor)
@@ -49,7 +49,7 @@ class TestMaxPooling(unittest.TestCase):
         self.assertLessEqual(np.sum(difference), 1e-6)
 
     def test_gradient_overlapping_stride(self):
-        self.layers[0] = self.MaxPooling((2, 1), (2, 2))
+        self.layers[0] = self.MaxPooling(neighborhood=(2, 2), stride=(2, 1))
         self.layers[2] = self.FullyConnected(24, self.categories, 0.)
 
         difference = Helpers.gradient_check(self.layers, self.input_tensor, self.label_tensor)
@@ -58,7 +58,7 @@ class TestMaxPooling(unittest.TestCase):
 
     def test_gradient_subsampling_stride(self):
 
-        self.layers[0] = self.MaxPooling((3, 2), (2, 2))
+        self.layers[0] = self.MaxPooling(neighborhood=(2, 2), stride=(3, 2))
         self.layers[2] = self.FullyConnected(6, self.categories, 0.)
 
         difference = Helpers.gradient_check(self.layers, self.input_tensor, self.label_tensor)
@@ -66,7 +66,7 @@ class TestMaxPooling(unittest.TestCase):
         self.assertLessEqual(np.sum(difference), 1e-6)
 
     def test_layout_preservation(self):
-        pool = self.MaxPooling((1, 1), (1, 1))
+        pool = self.MaxPooling(neighborhood=(1, 1), stride=(1, 1))
         input_tensor = np.array(range(np.prod(self.input_shape) * self.batch_size), dtype=np.float)
         input_tensor = input_tensor.reshape(self.batch_size, *self.input_shape)
         output_tensor = pool.forward(input_tensor)
@@ -74,7 +74,7 @@ class TestMaxPooling(unittest.TestCase):
 
     def test_expected_output_valid_edgecase(self):
         input_shape = (1, 3, 3)
-        pool = self.MaxPooling((2, 2), (2, 2))
+        pool = self.MaxPooling(neighborhood=(2, 2), stride=(2, 2))
         batch_size = 2
         input_tensor = np.array(range(np.prod(input_shape) * batch_size), dtype=np.float)
         input_tensor = input_tensor.reshape(batch_size, *input_shape)
@@ -85,7 +85,7 @@ class TestMaxPooling(unittest.TestCase):
 
     def test_expected_output(self):
         input_shape = (1, 4, 4)
-        pool = self.MaxPooling((2, 2), (2, 2))
+        pool = self.MaxPooling(neighborhood=(2, 2), stride=(2, 2))
         batch_size = 2
         input_tensor = np.array(range(np.prod(input_shape) * batch_size), dtype=np.float)
         input_tensor = input_tensor.reshape(batch_size, *input_shape)
